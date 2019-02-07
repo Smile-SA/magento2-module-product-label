3b98497b3ce779b255262db9add0869fff867e4c
Romain Ruaud <romain.ruaud@smile.fr>
2019-02-07 12:23:34 +0100

    Initial commit.

---
+++ registration.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\ProductLabel
+ * @author    Houda El Rhozlane <houda.elrhozlane@smile.fr>
+ * @copyright 2019 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Smile_ProductLabel',
+    __DIR__
+);
